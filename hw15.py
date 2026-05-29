ed6ea25dbbd8461fdc93fee1cf36bd9356f98c60
Elena_Ukrainets <elenaukrainec28@gmail.com>
2026-05-29 19:16:05 +0200

    hw15

---
+++ hw15.py
@@ -0,0 +1,55 @@
+""" 01. Одно слово
+
+Напишите программу, которая
+- обрабатывает список из строк
+- и удаляет все строки, содержащие более одного слова,
+- а также преобразует оставшиеся строки к нижнему регистру.
+
+ВАЖНО: НЕ создать новый список, а УДАЛИТЬ лишние элементы из существующего!
+
+Данные:
+text_list = ["Hello", "Python Programming", "World", "Advanced Topics", "Simple"]
+Пример вывода:
+Обработанный список: ['hello', 'world', 'simple']
+"""
+
+text_list = ["Hello", "Python Programming", "World", "Advanced Topics", "Simple"]
+for word in text_list.copy():
+    if " " in word:
+        text_list.remove(word)
+    else:
+        i = text_list.index(word)
+        text_list[i] = word.lower()
+print(text_list)
+""" 02. Обновление цен товаров
+
+Дан список товаров с ценами.
+Программа должна
+- применить скидку к каждому товару
+- и добавить в список элемент с новой ценой.
+- В конце вывести таблицу с новой ценой.
+
+Данные:
+products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
+Пример вывода:
+Введите скидку (в процентах): 17
+
+Товар          Старая цена    Новая цена
+Laptop            1200.00$       996.00$
+Mouse               25.00$        20.75$
+Keyboard            75.00$        62.25$
+Monitor            200.00$       166.00$
+"""
+products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
+discount = int(input("Введите скидку (в процентах): "))
+
+for i in range(len(products)):
+    old_price = products[i][1]
+    new_price = old_price * (1 - discount / 100)
+    products[i].append(new_price)
+
+print(f"\n{'Товар':<15}{'Старая цена':<15}{'Новая цена':<15}")
+
+for item in products:
+    print(f"{item[0]:<15}{item[1]:>10.2f}$ {item[2]:>12.2f}$")
+
